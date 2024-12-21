a58a7b957b8a83d95c8c5dd9459011429b3a2fb1
Ded2ec <ded2ec@Ded2ecs-iMac.local>
2024-12-22 00:27:03 +0900

    add Routing&updateFunctionNav

--- app/backoffice/layout.tsx
+++ app/backoffice/layout.tsx
@@ -1,17 +1,22 @@
-import TopNav from "../components/top-nav";
+
 import { Sidebar } from "../components/sidebar";
+import { TopNav } from "../components/top-nav";
 
-export default function BackofficeLayout({ children }: { children: React.ReactNode }) {
+export default function DashboardLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <div className="min-h-screen bg-gray-100">
-      <TopNav />
+      <div className="min-h-screen bg-gray-100">
+          <TopNav/>
 
-      <div className="bg-gray-880">
-        <Sidebar />
-        <main className="flex-1 p-6 bg-gradient-to-t from-gray-600 to-gray-950 rounded-tl-3xl">
-            {children}
-        </main>
+          <div className="flex bg-gray-800">
+              <Sidebar />
+              <main className="flex-1 p-6 bg-gradient-to-t from-gray-600 to-gray-950 rounded-tl-3xl">
+                  {children}
+              </main>
+          </div>
       </div>
-    </div>
   );
 }
