3b3e4e385a37468dbe98ca3e1fa0290200c99fa9
Ded2ec <ded2ec@Ded2ecs-iMac.local>
2024-12-22 21:06:02 +0900

    add page create:user&edituser

--- app/config.ts
+++ app/config.ts
@@ -1,6 +1,20 @@
+import Swal from 'sweetalert2';
+
 export const config = {
     apiUrl: 'http://localhost:3001',
-    tokenKey: 'token_bun_service'
+    tokenKey: 'token_bun_service',
+    confirmDialog: () => {
+        return Swal.fire({
+        title: 'ยืนยันการลบข้อมูล?',
+        text: "ข้อมูลจะถูกลบออกจากฐานข้อมูล",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#d33',
+        cancelButtonColor: '#3085d6',
+        cancelButtonText: 'ยกเลิก',
+        confirmButtonText: 'ยืนยัน'
+   });
+}
 }
 
 export default config; 
